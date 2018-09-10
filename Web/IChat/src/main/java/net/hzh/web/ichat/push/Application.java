a1d0df7e49ab08a204f72e3e304b51c069257bb9
huangzhaohao <283051928@qq.com>
2018-09-10 14:48:32 +0800

    完成第一个Restful接口

--- Web/IChat/src/main/java/net/hzh/web/ichat/push/Application.java
+++ Web/IChat/src/main/java/net/hzh/web/ichat/push/Application.java
@@ -1,5 +1,24 @@
 package net.hzh.web.ichat.push;
 
-public class Application {
+import com.fasterxml.jackson.jaxrs.json.JacksonJsonProvider;
+import net.hzh.web.ichat.push.service.AccountService;
+import org.glassfish.jersey.server.ResourceConfig;
 
+import java.util.logging.Logger;
+
+/**
+ * @author hzh
+ */
+public class Application extends ResourceConfig {
+    public Application(){
+        // 注册逻辑处理的包
+        //packages("net.hzh.web.ichat.push.service");
+        packages(AccountService.class.getPackage().getName());
+
+        // 注册Json解析器
+        register(JacksonJsonProvider.class);
+
+        // 注册日志打印输出
+        register(Logger.class);
+    }
 }
